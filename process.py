1f020872e8659d824d2238019d47ac614df35dec
TiagoOlivaCosta <tiago.oliva.costa@gmail.com>
2020-07-03 12:57:30 -0300

    fix: change C matrix

--- process.py
+++ process.py
@@ -5,15 +5,15 @@ import control
 
 T = 0.1  # sampling time
 sigma_w = 1  # process variance (m/s)^2
-# State transition matrix, F
-F = np.array([[1, T],
+# State transition matrix, A
+A = np.array([[1, T],
               [0, 1]])
 B = np.array([[0], [T]])  # input matrix
 # Process Covariance matrix, Q
 Q = np.array([[1./3*T**3, 0.5*T**2],
               [0.5*T**2, T]])*sigma_w**2
-C = np.array([[1, 0]])  # observation matrix
-sys = control.ss(F, B, C, 0, T)
+C = np.eye(2)
+sys = control.ss(A, B, C, 0, T)
 
 if __name__ == "__main__":
     time = np.arange(0, 21, T)
