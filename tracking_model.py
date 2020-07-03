4eec2bff6da07231abfdcaf90766d7f63d5df251
TiagoOlivaCosta <tiago.oliva.costa@gmail.com>
2020-07-03 04:20:28 -0300

    refactor: implement speedometer as a standalone class to make it have fixed bias

--- tracking_model.py
+++ tracking_model.py
@@ -6,14 +6,18 @@ x_dot = 20*np.ones(len(t))  # m/s
 x = t*x_dot  # position (m)
 
 
-def speed(x_dot):
-    # Speedometer measurement model
-    # consider speeds from 0 to 120 km/h
-    sigma_nu = 0.01*120  # km/h
-    sigma_nu = sigma_nu/3.6  # m/s
-    nu = np.random.randn()*sigma_nu
-    b = 1 + (np.random.random()-0.5)*0.1
-    return b*x_dot + nu
+class Speedometer:
+    def __init__(self):
+        self.bias = 1 + (np.random.random()-0.5)*0.1
+        print("Speedometer bias: ", self.bias)
+
+    def speed(self, x_dot):
+        # Speedometer measurement model
+        # consider speeds from 0 to 120 km/h
+        sigma_nu = 0.01*120  # km/h
+        sigma_nu = sigma_nu/3.6  # m/s
+        nu = np.random.randn()*sigma_nu
+        return self.bias*x_dot + nu
 
 
 def gps_measurement(x):
@@ -31,10 +35,13 @@ def msq_error(x, y):
 
 # speed
 plt.plot(t, x_dot)
-y_dot = [speed(i) for i in x_dot]
+speedometer = Speedometer()
+y_dot = [speedometer.speed(i) for i in x_dot]
 plt.plot(t, y_dot)
-error = np.sqrt(msq_error(x_dot, y_dot))
-print(f"Speedometer error: {error:.2f} m/s")
+msqerror = np.sqrt(msq_error(x_dot, y_dot))
+avgerror = np.average(y_dot-x_dot)
+print(f"Sqrt of MSq speed error: {msqerror:.2f} m/s")
+print(f"Average error: {avgerror:.2f} m/s")
 plt.show()
 
 # position
@@ -42,5 +49,7 @@ y = [gps_measurement(i) for i in x]
 plt.plot(t, x)
 plt.plot(t, y)
 plt.show()
-error = np.sqrt(msq_error(x, y))
-print(f"GPS error: {error:.2f} m")
+msqerror = np.sqrt(msq_error(x, y))
+avgerror = np.average(y-x)
+print(f"Sqrt of MSq  GPS error: {msqerror:.2f} m/s")
+print(f"Average GPS error: {avgerror:.2f} m/s")
