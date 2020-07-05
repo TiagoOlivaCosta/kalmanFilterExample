7c960c605102cb5e807c09237a8b849347a716d7
Tiago Oliva <tiago.oliva.costa@gmail.com>
2020-07-05 20:56:17 -0300

    feat: add unbiased mode to speedometer

--- sensors.py
+++ sensors.py
@@ -3,17 +3,23 @@ import matplotlib.pyplot as plt
 
 
 class Speedometer:
-    def __init__(self):
-        self.bias = 1 + (np.random.random()-0.5)*0.1
-        print("Speedometer bias: ", self.bias)
+    def __init__(self, mode="biased"):
+        assert type(mode) is str
+        self.mode = mode.lower()
+        if self.mode == "biased":
+            self.bias = 1 + (np.random.random()-0.5)*0.1
+            print("Speedometer bias: ", self.bias)
 
     def measurement(self, x_dot):
         # Speedometer measurement model
         # consider speeds from 0 to 120 km/h
-        sigma_nu = 0.1*120  # km/h
+        sigma_nu = 0.05*120  # km/h
         sigma_nu = sigma_nu/3.6  # m/s
         nu = np.random.randn()*sigma_nu
-        return self.bias*x_dot + nu
+        if self.mode == "biased":
+            return self.bias*x_dot + nu
+        elif self.mode == "unbiased":
+            return x_dot + nu
 
 
 class GPS:
@@ -47,6 +53,19 @@ if __name__ == "__main__":
     avgerror = np.average(y_dot-x_dot)
     print(f"Sqrt of MSq speed error: {msqerror:.2f} m/s")
     print(f"Average error: {avgerror:.2f} m/s")
+    plt.show()
+
+    # speed biased vs unbiased 
+    plt.plot(t, x_dot)
+    speedometer_unbiased = Speedometer(mode="unbiased")
+    y_dot = [speedometer.measurement(i) for i in x_dot]
+    y_dot_unbiased = [speedometer_unbiased.measurement(i) for i in x_dot]
+    plt.plot(t, y_dot)
+    plt.plot(t, y_dot_unbiased)
+    msqerror = np.sqrt(msq_error(x_dot, y_dot_unbiased))
+    avgerror = np.average(y_dot_unbiased-x_dot)
+    print(f"Sqrt of MSq speed error (unbiased): {msqerror:.2f} m/s")
+    print(f"Average error (unbiased): {avgerror:.2f} m/s")
     plt.show()
 
     # position
